2cbfb803f329682bd8755a493c4f851de58eb17c
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-06 15:52:35 +0100

    Initial commit

---
+++ src/main/java/com/bae/SpringAccountNumGen1Application.java
@@ -0,0 +1,13 @@
+package com.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAccountNumGen1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAccountNumGen1Application.class, args);
+	}
+
+}
